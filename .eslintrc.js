f519f22ceb2fb9514735290cc9b5f297f3e73a29
Daniel Lopez <daniel@bldrscove.com>
2021-12-02 18:13:22 -0500

    Set up airbnb-typescript. Include import as extends plugin.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,45 @@
 module.exports = {
-	extends: [
-		"react-app",
-		"prettier",
-		"plugin:jsx-a11y/recommended",
-	],
-	plugins: ["jsx-a11y", "prettier"],
-	rules: {
-		"react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".tsx"] }],
-	},
-	overrides: [
-		{
-			files: ["**/*.ts?(x)"],
-			rules: {},
-		},
-	],
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "jest",
+  ],
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        endOfLine: "auto",
+      }
+    ],
+    "arrow-body-style": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".ts", ".jsx", ".tsx",] }],
+    "react/state-in-constructor": "off",
+  },
+  settings:{
+    "import/ignore": ["node_modules"]
+  }
 };
